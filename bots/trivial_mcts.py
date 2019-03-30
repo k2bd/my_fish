566018ae50cadd4a861a1c3892626e4274462f8b
Kevin Duff <kevinkelduff@gmail.com>
2019-03-30 20:43:42 +0000

    Bot actually added

---
+++ bots/trivial_mcts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from mcts import mcts
+
+class MctsPlayer:
+    def __init__(self, pnum, time_lim_ms=5000):
+        self.pnum = pnum
+        self.brain = mcts(timeLimit=time_lim_ms)
+
+    def get_move(self, initialState):
+        initialState.PROTAGONIST = self.pnum
+        return self.brain.search(initialState=initialState)
